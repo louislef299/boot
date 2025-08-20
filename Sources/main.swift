fd1d5b1296d80f6f3dd121c9f1b11be12945193a
Louis Lefebvre <lefebl4@medtronic.com>
2025-08-20 15:12:12 -0500

    feat: Add ParsableCommand dep

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,4 +1,11 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import ArgumentParser
 
-print("Hello, world!")
+@main
+struct Boot: ParsableCommand {
+  @Option(help: "Specify the input")
+  public var input: String
+
+  public func run() throws {
+    print(self.input)
+  }
+}
