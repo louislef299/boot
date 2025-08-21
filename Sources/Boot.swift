5a2dbf46131a3c1207b391abf7ad6a761d275fa1
Louis Lefebvre <lefebl4@medtronic.com>
2025-08-21 14:55:42 -0500

    feat: Boot files to local .vscode

--- Sources/Boot.swift
+++ Sources/Boot.swift
@@ -1,11 +1,36 @@
-struct Boot {
-    enum BootError: Error {
-        case fileNotFound
+import ArgumentParser
+import Foundation
+
+@main
+struct Boot: ParsableCommand {
+  static let configuration = CommandConfiguration(
+    abstract: "Boot your files!")
+
+  @Argument(
+    help: "File to be parsed.",
+    transform: URL.init(fileURLWithPath:)
+  )
+  var file: URL
+
+  mutating func run() throws {
+    print("\nReceived file \(file)\n")
+    let destinationDirectory = URL(fileURLWithPath: "./.vscode")
+    let fileManager = FileManager.default
+
+    // Create destination directory if it doesn't exist
+    if !fileManager.fileExists(atPath: destinationDirectory.path) {
+        try? fileManager.createDirectory(at: destinationDirectory, withIntermediateDirectories: true)
     }
 
-    var bootDir: String
+    // Set up destination file path
+    let destinationFilePath = destinationDirectory.appendingPathComponent(file.lastPathComponent)
 
-    func boot(_ file: String) throws {
-        throw BootError.fileNotFound
+    // Move the file
+    do {
+        try fileManager.moveItem(at: file, to: destinationFilePath)
+        print("Successfully moved file to \(destinationFilePath.path)")
+    } catch {
+        print("Error moving file: \(error)")
     }
+  }
 }
