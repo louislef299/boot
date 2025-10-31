38d7bc75cf6d1f545b0a210b92e3ce30f72fa079
Louis Lefebvre <lefebl4@medtronic.com>
2025-10-31 12:46:19 -0500

    chore: Migrate cli to dedicated folder

--- cli/Package.swift
+++ cli/Package.swift
@@ -16,7 +16,7 @@ let package = Package(
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
             ],
-            path: "Sources/App/CLI"),
+            path: "Sources"),
         .testTarget(
             name: "bootTests",
             dependencies: ["boot"],
